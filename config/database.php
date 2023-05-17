67eea1b1788e72c9c179ba02f8ea5ce90dc55d9a
Palash Sharma <palash.sharma2002@gmail.com>
2023-05-17 17:37:45 -0400

    fix: yarn and db config

--- config/database.php
+++ config/database.php
@@ -61,11 +61,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => 'localhost',
-            'port' => 5432,
-            'database' => 'quickbib',
-            'username' => 'postgres',
-            'password' => '',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'quickbib'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
